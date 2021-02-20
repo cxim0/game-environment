cb3028ff39978683f2392657bdee2e9652884787
cxim0 <79256841+cxim0@users.noreply.github.com>
2021-02-20 19:55:05 +0800

    Update HugeInt.h

--- HugeInt.h
+++ HugeInt.h
@@ -8,12 +8,12 @@ public:
 //构造函数
 HugeInt(int a=0){
     this->i.push_back(a);
-    this->ii=this->i.begin;
+    this->ii=this->i.begin();
 }
 //禁用拷贝构造
 HugeInt(HugeInt){
     this->i.push_back(0);
-    this->ii=this->i.begin
+    this->ii=this->i.begin();
 }
 //int赋值
 HugeInt& operator=(int a){
@@ -23,6 +23,10 @@ HugeInt& operator=(int a){
 //long long int赋值
 HugeInt& operator=(long long int A){
     int a=GET_BYTE(A,1)|GET_BYTE(A,2)|GET_BYTE(A,3)|GET_BYTE(A,4),b=GET_BYTE(A,5)|GET_BYTE(A,6)|GET_BYTE(A,7)|GET_BYTE(A,8);
+    *(this->ii)=a;
+    if(
+    
+    this->ii=this->i.begin();
     return *this;
 }
 
