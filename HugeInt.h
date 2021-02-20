51b1288023c410e45eb98002feb3a507357150bc
cxim0 <79256841+cxim0@users.noreply.github.com>
2021-02-20 19:46:37 +0800

    Update HugeInt.h

--- HugeInt.h
+++ HugeInt.h
@@ -12,7 +12,7 @@ HugeInt(int a=0){
 }
 //禁用拷贝构造
 HugeInt(HugeInt){
-    this->i.push_back(a);
+    this->i.push_back(0);
     this->ii=this->i.begin
 }
 //int赋值
@@ -21,9 +21,11 @@ HugeInt& operator=(int a){
     return *this;
 }
 //long long int赋值
-HugeInt& operator=(long long int a){
-    
+HugeInt& operator=(long long int A){
+    int a=GET_BYTE(A,1)|GET_BYTE(A,2)|GET_BYTE(A,3)|GET_BYTE(A,4),b=GET_BYTE(A,5)|GET_BYTE(A,6)|GET_BYTE(A,7)|GET_BYTE(A,8);
+    return *this;
 }
+
 
 
 
