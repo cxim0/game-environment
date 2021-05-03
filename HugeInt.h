f1b04f336f07acb54a84a4f72c807fa9b22787d0
cxim0 <79256841+cxim0@users.noreply.github.com>
2021-05-03 11:55:07 +0800

    Update HugeInt.h

--- HugeInt.h
+++ HugeInt.h
@@ -2,12 +2,32 @@
 
 #include "byte_player.h"
 #include "bit_player.h"
+#include <vector>
 
 class HugeInt{
 public:
-
+    HugeInt(__int64 a=0){
+        __int16*p=(__int16*)&a;
+        v.push_back(*p);
+        v.push_back(*(p++));
+        v.push_back(*(p++));
+        v.push_back(*(p++));
+    }
+    HugeInt(HugeInt a){
+        *this=a;
+    }
+    HugeInt& opertaor=(HugeInt a){
+        
+        return *this;
+    }
+    HugeInt& operator=(__int64 a){
+        *this=HugeInt(a);
+        return *this;
+    }
 protected:
-
+    vector<__int16>v;
+    vector<__int16>::iterator it16;
+    vector<__int8>::irerator it8;
 private:
 
 };
