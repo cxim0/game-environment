d24e2698f794bc09192f48d198e0148ec1396265
cxim0 <79256841+cxim0@users.noreply.github.com>
2021-09-25 12:09:49 +0800

    Update HugeInt.h

--- HugeInt.h
+++ HugeInt.h
@@ -1,42 +1,26 @@
 #program once
 
-#include "byte_player.h"
-#include "bit_player.h"
-#include <vector>
-
-usingnamespace std;
+#include "memory_player.cpp"
+#include "HugeInt.cpp"
 
 class HugeInt{
 public:
-    HugeInt(__int64 a=0){
-        __int16*p=(__int16*)&a;
-        v.push_back(*p);
-        v.push_back(*(p++));
-        v.push_back(*(p++));
-        v.push_back(*(p++));
-    }
-    HugeInt(HugeInt a){
-        this.v=vector(a.v);
-    }
-    HugeInt& opertaor=(HugeInt a){
-        
-        return *this;
-    }
-    HugeInt& operator=(__int64 a){
-        *this=HugeInt(a);
-        return *this;
-    }
-    HugeInt operator+(HugeInt a){
-        __int16 t=0;
-        //...
-    }
-    HugeInt operator+(__int64 a){
-        (*this)+HugeInt(a);
-    }
+    HugeInt(void* data,int length);
+    template<size_t L>
+    HugeInt(Data<L>);
+    HugeInt();
+    HugeInt& operator=();
+    const HugeInt operator+(HugeInt);
+    const HugeInt operator-(HugeInt);
+    const HugeInt operator*(HugeInt);
+    const HugeInt operator/(HugeInt);
+    const HugeInt operator<<(int);
+    const HugeInt operator>>(int);
+    const HugeInt operator^(HugeInt);
+    
+    const HugeInt operator~();
 protected:
-    vector<__int16>v;
-    vector<__int16>::iterator it16;
-    vector<__int8>::irerator it8;
+    
 private:
-
+    
 };
