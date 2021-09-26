86f5bb5f9be66065f6e2adbd4c0736ac11abece0
cxim0 <79256841+cxim0@users.noreply.github.com>
2021-09-26 21:34:00 +0800

    Update HugeInt.h

--- HugeInt.h
+++ HugeInt.h
@@ -1,30 +1,42 @@
-#proaram once
+#program once
 
 #include "memory_player.cpp"
 
 class HugeInt{
 public:
-    HugeInt(void* data,int length);
-    template<size_t L>
-    HugeInt(Data<L>);
-    HugeInt();
-    HugeInt& operator=();
-    const HugeInt operator+(HugeInt);
-    const HugeInt operator-(HugeInt);
-    const HugeInt operator*(HugeInt);
-    const HugeInt operator/(HugeInt);
-    const HugeInt operator<<(int);
-    const HugeInt operator>>(int);
-    const HugeInt operator^(HugeInt);
-    const HugeInt operator&(HugeInt);
-    const HugeInt operator|(HugeInt);
-    const HugeInt operator~();
-    const HugeInt to_length(int);
+    HugeInt(void* data,int length);
+    template<size_t L>
+    HugeInt(Data<L>);
+    HugeInt(int);
+    HugeInt();
+    HugeInt& operator=();
+    HugeInt& operator+=(HugeInt);
+    HugeInt& operator-=(HugeInt);
+    HugeInt& operator*=(HugeInt);
+    HugeInt& operator/=(HugeInt);
+    HugeInt& operator<<=(signed int);
+    HugeInt& operator>>=(signed int);
+    HugeInt& operator^=(HugeInt);
+    HugeInt& operator&=(HugeInt);
+    HugeInt& operator|=(HugeInt);
+    const HugeInt operator+(HugeInt);
+    const HugeInt operator-(HugeInt);
+    const HugeInt operator*(HugeInt);
+    const HugeInt operator/(HugeInt);
+    const HugeInt operator<<(signed int);
+    const HugeInt operator>>(signed int);
+    const HugeInt operator^(HugeInt);
+    const HugeInt operator&(HugeInt);
+    const HugeInt operator|(HugeInt);
+    const HugeInt operator~();
+    const HugeInt& to_length(int);
 protected:
-    void length_to();
+    void length_to();
+    memory_player::int8*data8();
+    memory_player::int16*data16();
 private:
-    void*data;
-    unsigned int length;
+    void*data;
+    unsigned int length;
 };
 
 #include "HugeInt.cpp"
