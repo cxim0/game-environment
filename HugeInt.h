5f4f81f57fbe2d2ebcccd16f540c7f6036ee890a
cxim0 <79256841+cxim0@users.noreply.github.com>
2021-10-17 20:39:47 +0800

    Update HugeInt.h

--- HugeInt.h
+++ HugeInt.h
@@ -1,42 +1,65 @@
-#program once
+#pragam once
 
-#include "memory_player.cpp"
+#include <stdint.h>
 
-class HugeInt{
+class HugeInt final
+{
 public:
-    HugeInt(void* data,int length);
-    template<size_t L>
-    HugeInt(Data<L>);
-    HugeInt(int);
-    HugeInt();
-    HugeInt& operator=();
-    HugeInt& operator+=(HugeInt);
-    HugeInt& operator-=(HugeInt);
-    HugeInt& operator*=(HugeInt);
-    HugeInt& operator/=(HugeInt);
-    HugeInt& operator<<=(signed int);
-    HugeInt& operator>>=(signed int);
-    HugeInt& operator^=(HugeInt);
-    HugeInt& operator&=(HugeInt);
-    HugeInt& operator|=(HugeInt);
-    const HugeInt operator+(HugeInt);
-    const HugeInt operator-(HugeInt);
-    const HugeInt operator*(HugeInt);
-    const HugeInt operator/(HugeInt);
-    const HugeInt operator<<(signed int);
-    const HugeInt operator>>(signed int);
-    const HugeInt operator^(HugeInt);
-    const HugeInt operator&(HugeInt);
-    const HugeInt operator|(HugeInt);
-    const HugeInt operator~();
-    const HugeInt& to_length(int);
+    HugeInt(void* d,int byte_len){
+        data=new uint8_t[byte_len];
+        for(uint32_t i=0;i<byte_len;i++){
+            *((uint8_t*)(data+i))=
+            *((uint8_t*)(d+i));
+        }
+        size=byte_len;
+    }
+    HugeInt(uint64_t i){
+        data=(void*)new uint8[8];
+        (*(uint64_t*)data)=i;
+        size=8;
+    }
+    HugeInt(HugeInt& source){
+        //unsuggested to use except you know what you are doing
+        data=source.data;
+        size=source.size;
+    }
+    HugeInt(){size=0;}
+    ~HugeInt(){delete[] data;}
+    HugeInt& operator=(const HugeInt&);
+    HugeInt& operator+=(const HugeInt&);
+    HugeInt& operator-=(const HugeInt&);
+    HugeInt& operator*=(const HugeInt&);
+    HugeInt& operator/=(const HugeInt&);
+    HugeInt& operator<<=(const int);
+    HugeInt& operator>>=(const int);
+    HugeInt& operator^=(const HugeInt&);
+    HugeInt& operator&=(const HugeInt&);
+    HugeInt& operator|=(const HugeInt&);
+    HugeInt operator+(const HugeInt&) const;
+    HugeInt operator-(const HugeInt&) const;
+    HugeInt operator*(const HugeInt&) const;
+    HugeInt operator/(const HugeInt&) const;
+    HugeInt operator <<(const int) const;
+    HugeInt operator >>(const int) const;
+    HugeInt operator^(const HugeInt&) const;
+    HugeInt operator&(const HugeInt&) const;
+    HugeInt operator|(const HugeInt&) const;
+    HugeInt operator~() const;
+    size_t size()const{
+        return size;
+    }
+    HugeInt to_length(const size_t) const;
 protected:
-    void length_to();
-    memory_player::int8*data8();
-    memory_player::int16*data16();
+    void length_to(const size_t);
+    inline uint8_t*data8()const{
+        return (uint8_t*)data;
+    }
+    inline uint16_t*data16()const{
+        return (uint16_t*)data;
+    }
 private:
     void*data;
-    unsigned int length;
+    size_t size;
 };
 
 #include "HugeInt.cpp"
